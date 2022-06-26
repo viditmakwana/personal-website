ac5d347a21f8b7e40521cf153a4096b6714d31a4
Vidit Makwana <viditmakwana@Vidits-MacBook-Pro-2.local>
2022-06-26 01:37:57 -0400

    updated with content

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js"],
+  theme: {
+    extend: {},
+    colors: {
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      mono: ['Inconsolata'],
+    },
+  },
+  plugins: [],
+}
