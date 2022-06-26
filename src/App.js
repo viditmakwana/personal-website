0785dfe379d6ab892d7940d50245d12e420eb780
Vidit Makwana <viditmakwana@Vidits-MacBook-Pro-2.local>
2022-06-26 13:38:12 -0400

    adjusted wording and resume

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
 
                                 <svg class="h-6 w-6 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <a href="https://www.github.com/viditmakwana" target="_blank" rel="noreferrer noopener">
+                                    <a href="https://www.github.com/viditmakwana" target="_blank" rel="noreferrer noopener" alt="git">
                                         <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 
                                         6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 
                                         13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 
@@ -80,23 +80,22 @@ function App() {
                     <div></div>
                     <div className='container text-white text-md md:text-lg font-mono col-span-5 md:col-span-4 place-items-start'>
                         <div className='container p-6'>
-                            <p className='overflow-hidden text-justify'>
-                                My name is Vidit Makwana and I am an incoming senior at the University of Maryland. I am a recipient of
-                                the Presidential Scholarship and I am a part of the <a span class="responsive"
+                            <p className='overflow-hidden'>
+                                Welcome! My name is Vidit and I'm an incoming senior at the University of Maryland. Besides receiving
+                                the Presidential Scholarship and maintaining my <a span class="responsive"
                                     href="https://cmns.umd.edu/news-events/features/4529" target="_blank" rel="noreferrer noopener">
-                                    Dean's List</a>. I was recently inducted into the <a span class="responsive" href="https://mortarboard.org/about/" target="_blank" rel="noreferrer noopener">Mortar Board Honor Society</a>.
-                                    Also, I am the Director of Professional Development of <a span class="responsive"
-                                        href="https://ktpumd.com/" target="_blank" rel="noreferrer noopener"> Kappa Theta Pi </a>, the world's first co-educational professional technology fraternity. 
-                                I am pursuing software engineering and data science new grad positions.
-                                In the past, I had been a mentor and volunteer for <a span class="responsive" href="https://gotechnica.org/" target="_blank" rel="noreferrer noopener">Technica</a>, the world's largest
-                                hackathon for underrepresented genders. <br/><br/>This summer, I am working as a Software Engineering Intern at&nbsp;
+                                    Dean's List</a> status, I was recently inducted into the <a span class="responsive" href="https://mortarboard.org/about/" target="_blank" rel="noreferrer noopener">Mortar Board Honor Society</a>.
+                                    Additionally, I serve as the Director of Professional Development for the world's first co-educational professional technology fraternity,<a span class="responsive"
+                                        href="https://ktpumd.com/" target="_blank" rel="noreferrer noopener"> Kappa Theta Pi </a>. 
+                                My new grad job search is focused on data science and software engineering positions that allow me to gain a deeper understanding of the vast amount of data around me.
+                                As a mentor and volunteer, I've previously participated in <a span class="responsive" href="https://gotechnica.org/" target="_blank" rel="noreferrer noopener">Technica</a>, the largest
+                                hackathon for underrepresented genders in the world. <br/><br/>The summer so far has been an exciting experience working as a Software Engineering Intern at&nbsp;
                                 <a span class="responsive" href="https://www.wework.com/" target="_blank" rel="noreferrer noopener">WeWork</a> in New York City!
-                                Next semester, I will be studying Human-Computer Interactions and Compilers. I am also mentoring students in
-                                Relational Databases in PostgreSQL as a Teaching Assistant. I love to play and watch
-                                basketball, football, and tennis and talk about any potential business opportunities in my free time. I'm always willing to watch an
+                                During my free time, I enjoy playing and watching
+                                basketball, football, and tennis and traveling with friends. I'm always willing to watch an
                                 episode of <a span class="responsive" href="https://www.peacocktv.com/stream-tv/the-office" target="_blank" rel="noreferrer noopener">The Office</a> and currently, I am 
-                                catching up with <a span class="responsive" href="https://www.imdb.com/title/tt1190634/" target="_blank" rel="noreferrer noopener">The Boys</a>! I am always open to learning
-                                and contributing to projects, so let me know if you have any ideas!
+                                catching up with <a span class="responsive" href="https://www.imdb.com/title/tt1190634/" target="_blank" rel="noreferrer noopener">The Boys</a>! I am always looking for opportunities to learn
+                                and contribute to projects, so feel free to contact me if you have any ideas!
                             </p>
                         </div>
                     </div>
